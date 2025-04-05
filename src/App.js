c051c254804216ac04f997e48ad9f3c09ad63262
kidKorste <kidkorste@gmail.com>
2025-04-05 16:48:01 -0700

    flex box and animations on scroll

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 "use client"
-import { useState } from "react";
 import  grasshopper  from './assets/grasshopper.gif';
 
 export default function Home() {
 
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if(entry.isIntersecting){
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show')
+      }
+    })
+  });
+
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => observer.observe(el));
   
   return (
     <div className="container">
       <h1 className="text">Welcome to my home...</h1>
-        <div className="centerBlock">
-          <img src={grasshopper}/>
+        <div>
+          <img className="gif"src={grasshopper}/>
         </div>
         <p className="openText">Open 24/7</p>
-        <p className="pText">I have no idea what I am doing</p>
-        <p>test text</p>
-        
+        <p className="pText">I have no idea what I am doing</p>        
     </div>
 );
 }
