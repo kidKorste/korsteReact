cb7e883ec8395a8c60a957eb4a2f4ee86f3021c1
kidKorste <kidkorste@gmail.com>
2025-04-04 19:09:51 -0700

    add img

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 "use client"
 import { useState } from "react";
+import  grasshopper  from './assets/grasshopper.gif';
 
 export default function Home() {
 
@@ -7,6 +8,9 @@ export default function Home() {
   return (
     <div className="container">
       <h1 className="text">Welcome to my home...</h1>
+        <div className="centerBlock">
+          <img src={grasshopper}/>
+        </div>
         <p className="openText">Open 24/7</p>
         <p className="pText">I have no idea what I am doing</p>
     </div>
